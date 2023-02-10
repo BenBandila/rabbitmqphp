b86abd51fd76a8bbab630be10b4857e529cf6e08
BenBandila <benbandila11@gmail.com>
2023-02-10 12:49:51 -0500

    Added function to send login info to rabbit

--- sample/login.php
+++ sample/login.php
@@ -12,8 +12,18 @@ $response = "unsupported request type, politely FUCK OFF";
 switch ($request["type"])
 {
 	case "login":
-		$response = "login, yeah we can do that";
-	break;
+		//$response = "login, yeah we can do that";
+		$connection = new AMQPStreamConnection('localhost', 5672, 'test', 'test');//change localhost to broker host
+	        $channel = $connection->channel();
+	        $username = $_POST['username'];
+	        $password = $_POST['password'];
+	        $array = Array( "username" => $username, "password" => $password );
+	        //$json = json_encode($array);
+	        //file_put_content("login.json", $json);
+
+	        $msg = new AMQPMessage($array);
+	        $channel->basic_publish($msg);
+		break;
 }
 echo json_encode($response);
 exit(0);
